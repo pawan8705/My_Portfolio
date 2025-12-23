7e98a3a3b339b1edeed3f6cbc1ba54f5a9b5ff74
Pawan Tripathi <165460080+pawan8705@users.noreply.github.com>
2025-12-23 12:08:29 +0530

    Update constants.js

--- src/constants.js
+++ src/constants.js
@@ -187,6 +187,7 @@ export const projects = [
     tags: ["HTML", "CSS", "JavaScript", "Tailwind CSS", "React Js"],
     github: "https://github.com/pawan8705/My_Portfolio",
     webapp: "https://pawan-tripathi-portfolio.vercel.app",
+  },
   {
     id: 6,
     title: "Restaurant website",
