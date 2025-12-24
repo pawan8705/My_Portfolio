e3c235f8b51b127c0179b7fdbd4e6aae1c1b29d7
Pawan Tripathi <165460080+pawan8705@users.noreply.github.com>
2025-12-24 10:16:55 +0530

    Update About.jsx

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -45,7 +45,7 @@ const About = () => {
           </p>
           {/* Resume Button */}
           <a
-            href="https://drive.google.com/file/d/1JPmnWvBEuKjxevSgCSoRzsEUiN7Ho6ju/view?usp=drivesdk"
+            href="https://drive.google.com/file/d/17uSLW5W-0zEGDadyfQI6AfmWdjiZroye/view?usp=drivesdk"
             target="_blank"
             rel="noopener noreferrer"
             className="inline-block text-white py-3 px-8 rounded-full mt-5 text-lg font-bold transition duration-300 transform hover:scale-105"
